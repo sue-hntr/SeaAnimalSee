ee7591eff68724e42e95577bf191d74c75c8e77b
Suzanne Hunter <sue_huntr@yahoo.com>
2018-12-16 20:42:20 -0500

    10 gifs with captions works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,25 +3,16 @@
 
 ///////// Add 10 Gifs with Text at Bottom ////////////
 $(document).ready(function(){
-  
   $(".seaAnimal").on("click", function(event) {
     event.preventDefault();
     // console.log($(this));
     console.log("testing");
       //put URL into a variable to grab gif with 'data-name' in search
       var giphyURLApi = "https://api.giphy.com/v1/gifs/search?api_key=3zXCEbPoYOdjO9AcwJIkjuf5zi3OnxO5";
-      
       var searchIt = $(this).data('name');
-      // var searchTerm = "&q=" + searchIt;
-     var searchTerm = "&q=" + searchIt + "&limit=10"; 
-      console.log(searchTerm);
+      var searchTerm = "&q=" + searchIt + "&limit=10"; 
       var queryURL = giphyURLApi + searchTerm;
-      console.log(queryURL);
-
-    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=sJwaRrAUbqPzIBggUpsrUDnCrFgqTRQR&limit=5");
-    // xhr.done(function(data) { console.log("success got data", data); });
-
-
+   
     //ajax request that send get request to URL
     $.ajax({
       url: queryURL,
@@ -30,70 +21,76 @@ $(document).ready(function(){
 // Object of an Array of Objects
     //once there is a reponse what happens
       .then(function(response) {
-        var myObj = response.data;
-        var myObj2 = (myObj[1]["images"]["original"]["url"]);
-        // console.log(typeof myObj);
+        var imageObj = response.data;
+        var x = 0;
+        while(x < 10){
+          //JSON object URL information  
+            var imageAnimateUrl = (imageObj[x]["images"]["original"]["url"]);
+            var imageStillUrl = (imageObj[x]["images"]["original_still"]["url"]);
+            var ratingImage = (imageObj[x]["rating"]);
+          //create div container to keep each item together
+            var gifDiv = $('<div>');
 
-        // console.log(JSON.stringify(myObj[1]["images"]["original"]["url"]));
-        // var myObj2 = []
-        // var z = JSON.stringify("z: " + response);
-        // var o = response.data[0].url;
-        // console.log("array " + o);
-        // var e = JSON.stringify("e: " + response.data);
-        // console.log(response);
-        // console.log(e);
-      //from the JSON response pull the IMG information
-
-
-        // var z = JSON.stringify(response.data[0].url);
-        // var b = "" + z + "";
-        // var c = JSON.stringify(b);
-        // console.log("b " + b);
-        // var d = JSON.stringify(imageUrl);
-        // console.log(d);
-        // console.log(typeof response);  
-       
-
-      
-
-        // myObj = {
-        //   "name":"John",
-        //   "age":30,
-        //   "cars": {
-        //     "car1":"Ford",
-        //     "car2":"BMW",
-        //     "car3":"Fiat"
-        //   }
-        //  }
-// x = myObj.cars.car2;
-// // or:
-// x = myObj.cars["car2"];
-
-
-
-
-
-
-
-
-
-
-        //create an IMG tag to begin displaying image 
-        var animalImage = $("<img>");
-
-        //pull the unique URL info and insert a alt attribute for cat image 
-        animalImage.attr('src', myObj2);
-        // animalImage.attr("alt", "sea animal image");
-        console.log(JSON.stringify(animalImage));
-
+          //create an IMG tag to begin displaying still image
+            var animalImage = $("<img>");
+          //pull the unique URL info and insert a alt attribute for sea animal image 
+            animalImage.attr('src', imageStillUrl);
+            animalImage.attr("alt", "sea animal image");
+          //create the code for the captions tag  
+            var imageCaption = $('<p>').text("^ rating: " + ratingImage);
+            
+            gifDiv.prepend(animalImage);
+            gifDiv.append(imageCaption);
         //attach the cat images to the images ID
-        $("#images").prepend(animalImage);
-      
-      }); //close then
-    
+            $("#images").prepend(gifDiv);
+        //attach the rating text to the rating id
+            x++;
+        }
+      }); //close .then
   }); //close button
-
 }); //close doc ready
+
+/////THIS CODE WORKS PERFECTLY FOR ONE IMAGE. DO NOT DELETE UNTIL READY ////////
+// $(document).ready(function(){
+//   $(".seaAnimal").on("click", function(event) {
+//     event.preventDefault();
+//     // console.log($(this));
+//     console.log("testing");
+//       //put URL into a variable to grab gif with 'data-name' in search
+//       var giphyURLApi = "https://api.giphy.com/v1/gifs/search?api_key=3zXCEbPoYOdjO9AcwJIkjuf5zi3OnxO5";
+//       var searchIt = $(this).data('name');
+//       var searchTerm = "&q=" + searchIt + "&limit=10"; 
+//       var queryURL = giphyURLApi + searchTerm;
+   
+//     //ajax request that send get request to URL
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     })
+// // Object of an Array of Objects
+//     //once there is a reponse what happens
+//       .then(function(response) {
+//         var imageObj = response.data;
+//   //put loop here when ready
+//       //JSON object URL information  
+//         var imageAnimateUrl = (imageObj[1]["images"]["original"]["url"]);
+//         var imageStillUrl = (imageObj[1]["images"]["original_still"]["url"]);
+//         var ratingImage = (imageObj[1]["rating"]);
+//       //create an IMG tag to begin displaying still image 
+//         var animalImage = $("<img>");
+//       //pull the unique URL info and insert a alt attribute for sea animal image 
+//         animalImage.attr('src', imageStillUrl);
+//         animalImage.attr("alt", "sea animal image");
+//       //create the code for the captions tag  
+//         var imageCaption = $('<p>');
+//         imageCaption.html("rating: " + ratingImage);
+//     //attach the cat images to the images ID
+//         $("#images").prepend(animalImage);
+//     //attach the rating text to the rating id
+//         $('#rating').prepend(imageCaption);
+//       }); //close then
+//   }); //close button
+// }); //close doc ready
 
 ///////// Stop & Start Gifs ////////////
 
